55c03e6d8b0243344bd152c48f76d7b661932214
Mark Corrado <markcorrado@gmail.com>
2015-08-12 17:36:03 -0500

    Changed Fragment to ListFragment.  Added dummy data.

--- app/src/main/java/com/markcorrado/funwithgit/MainActivityFragment.java
+++ app/src/main/java/com/markcorrado/funwithgit/MainActivityFragment.java
@@ -1,22 +1,33 @@
 package com.markcorrado.funwithgit;
 
+import android.app.ListFragment;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends ListFragment {
 
     public MainActivityFragment() {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        stringArrayList.add("One");
+        stringArrayList.add("Two");
+        stringArrayList.add("Three");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, stringArrayList);
+        setListAdapter(adapter);
     }
 }
